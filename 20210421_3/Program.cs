496a021642ac56540240b7b5489e3842525eaf32
chan9279 <moonji9279@gmail.com>
2021-04-21 21:25:28 +0900

    랜덤 수 곱하기 문제

---
+++ 20210421_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _20210421_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int random1 = random.Next(2,10);
+            int random2 = random.Next(2 ,10);
+            Console.WriteLine(random1 + "X" + random2 +" = ?");
+            string input = Console.ReadLine();
+            int inputN3 = int.Parse(input);
+            if((random1 * random2) == inputN3)
+            {
+                Console.WriteLine("정답입니다. ");
+            }
+            else
+            {
+                Console.WriteLine("틀렸습니다. ");
+            }
+
+        }
+    }
+}
